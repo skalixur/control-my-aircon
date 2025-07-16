319d53b14e29f488ebe56f7ceec45b6eb28715ff
Skalixur <jmjmbo5@gmail.com>
2025-07-16 11:32:31 +0800

    Add more config and comments to config.example.h, add wifi functionality, create basic alive server, setup mDNS

--- config.example.h
+++ config.example.h
@@ -1,1 +1,12 @@
-decode_type_t protocol = decode_type_t::NEC;
+// Aircon protocol.
+// See possible values here: https://github.com/crankyoldgit/IRremoteESP8266/blob/42eead875d707b0f03f6d7f897b4dbb3b7607486/src/IRac.cpp
+const decode_type_t protocol = decode_type_t::COOLIX;
+// WiFi SSID.
+// Case sensitive. Must be exact.
+const char* ssid = "My WiFi SSID";
+// WiFi Password.
+// Leave empty for no password.
+const char* password = "asd123";
+// Server Port.
+const uint16_t port = 80;
+const char* mDnsName = "aircon"; // -> aircon.local
--- control-my-aircon.ino
+++ control-my-aircon.ino
@@ -1,15 +1,41 @@
 #include <Arduino.h>
 #include <IRremoteESP8266.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 #include <IRac.h>
 #include <IRutils.h>
 #include "config.h"
 
 const uint16_t kIrLed = D1;
+
+WiFiEventHandler gotIpEventHandler;
+ESP8266WebServer server(port);
 IRac ac(kIrLed);
+
+void handleRoot() {
+  server.send(200, "text/plain", "I am alive!\r\n");
+  Serial.println("Received request at root");
+}
+
+void handleGotIp(const WiFiEventStationModeGotIP& event) {
+  Serial.print("Successfully connected to ");
+  Serial.println(WiFi.SSID());
+  Serial.print("Local IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Hostname: ");
+  Serial.println(WiFi.hostname());
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
+}
 
 void setup() {
   Serial.begin(115200);
   delay(200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  gotIpEventHandler = WiFi.onStationModeGotIP(&handleGotIp);
 
   ac.next.protocol = protocol;
   ac.next.model = 1;
@@ -29,26 +55,53 @@ void setup() {
   ac.next.clean = false;
   ac.next.clock = -1;
   ac.next.power = false;
+
+  // Ensure WiFi is connected before anything else
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+
+  if (MDNS.begin(mDnsName)) {
+    Serial.println("MDNS responder started sucessfully");
+  }
+
+  server.on('/alive', HTTP_GET, []() {
+    server.send(200, "text/plain", "I am alive!")
+  });
+
+  server.method()
+
+  Serial.println();
+
+  server.begin();
 }
 
 void loop() {
 
   if (!ac.isProtocolSupported(protocol)) {
     Serial.println("Selected protocol is not supported by library!");
+    delay(10000);
     return;
   }
 
-  Serial.println("Protocol " + String(protocol) + " / " + typeToString(protocol) + " is supported.");
+  server.handleClient();
+  MDNS.update();
 
-  ac.next.power = true;
-  Serial.println("Turning the aircon on");
-  ac.sendAc();
-  delay(1000);
+  // Serial.println("Protocol " + String(protocol) + " / " + typeToString(protocol) + " is supported.");
 
-  ac.next.power = false;
-  Serial.println("Turning the aircon off");
-  ac.sendAc();
-  delay(1000);
+  // ac.next.power = true;
+  // Serial.println("Turning the aircon on");
+  // ac.sendAc();
+  // delay(5000);
 
-  Serial.println("Repeating\n");
+  // ac.next.power = false;
+  // Serial.println("Turning the aircon off");
+  // ac.sendAc();
+  // delay(5000);
+
+  // Serial.println("Repeating\n");
 }
